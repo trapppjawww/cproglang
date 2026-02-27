71ae8347ca86856cbd1b4bed29b1a8e48ee6c5ac
trapppjawww <238725676+trapppjawww@users.noreply.github.com>
2026-02-27 08:22:50 -0800

    ch 1: ex 18 wip

--- ch_1/ex_1-18.c
+++ ch_1/ex_1-18.c
@@ -2,15 +2,39 @@
 
 #include <stdio.h>
 
-#define MAXIMUM 100
+#define MAXIMUM 20
 
 int get_line(char s[], int lim);
 
 int main()
 {
-  int c, i;
+  int len;
   char line[MAXIMUM];
 
+  len = 0;
+
+  while ((len = get_line(line, MAXIMUM)) > 0) {
+    if (len == 1) continue;
+
+    int i;
+    for (i = len - 1; i >=0; --i) {
+      if (
+	  line[i] != ' ' &&
+	  line[i] != '\t' &&
+	  line[i] != '\n' &&
+	  line[i] != '\0'
+      ) {
+	break;
+      }
+      
+      if (line[i] == ' ' || line[i] == '\t') continue;
+    }
+    
+    line[i + 1] = '\n';
+    line[i + 2] = '\0';
+    
+    printf("%s", line);
+  }
 }
 
 int get_line(char s[], int lim)
@@ -18,7 +42,7 @@ int get_line(char s[], int lim)
   int i, c;
 
   for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) 
-    s[i];
+    s[i] = c;
 
   if (c == '\n') {
     s[i] = c;
